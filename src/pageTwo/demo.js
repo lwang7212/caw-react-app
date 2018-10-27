7ce9e7999d411ddbd97eea56d1611529161ccd57
lwang7212 <lwang7212@163.com>
2018-10-27 14:35:09 +0800

    多页面配置

---
+++ src/pageTwo/demo.js
@@ -0,0 +1,4 @@
+import  App from "../App";
+import style from "../App.css";
+import ReactDOM from "react-dom";
+ReactDOM.render(App, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,106 @@
+const path = require('path');
+//const _ = require('lodash');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-module.exports = {
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            },
-            {
-                test: /\.html$/,
-                use: [
-                    {
-                        loader: "html-loader",
-                        options: {minimize: true}
-                    }
-                ]
-            },
-            {
-                test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, "css-loader"]
+const _const_module = {
+    rules: [
+        {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: {
+                loader: "babel-loader"
             }
-        ]
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: "./src/index.html",
-            filename: "./index.html"
-        }),
-        new MiniCssExtractPlugin({
-            filename: "[name].css",
-            chunkFilename: "[id].css"
-        })
+        },
+        {
+            test: /\.html$/,
+            use: [
+                {
+                    loader: "html-loader",
+                    options: {minimize: true}
+                }
+            ]
+        },
+        {
+            test: /\.css$/,
+            use: [MiniCssExtractPlugin.loader, "css-loader"]
+        }
     ]
 };
+module.exports = [
+    {
+        // 入口文件
+        entry: {
+            pageOne: './src/pageOne/pageOne.js',
+        },
+        output: {
+            filename: 'script/[name].js',
+            path: path.resolve(__dirname, 'dist'),
+        },
+        module: _const_module,
+        plugins: [
+
+            new HtmlWebPackPlugin({
+                files: {
+                    js: ["script/pageOne.js"]
+                },
+                template: "./src/pageOne/pageOne.html",
+                filename: "./pageOne/pageOne.html"
+            }),
+            new MiniCssExtractPlugin({
+                filename: "[name].css",
+                chunkFilename: "[id].css"
+            })
+        ]
+    },
+    {
+        // 入口文件
+        entry: {
+
+            pageTwo: './src/pageTwo/pageTwo.js'
+
+        },
+        output: {
+            filename: 'script/[name].js',
+            path: path.resolve(__dirname, 'dist'),
+        },
+        module: _const_module,
+        plugins: [
+
+            new HtmlWebPackPlugin({
+                files: {
+                    js: ["script/pageTwo.js"]
+                },
+                template: "./src/pageTwo/pageTwo.html",
+                filename: "./pageTwo/pageTwo.html"
+            }),
+            new MiniCssExtractPlugin({
+                filename: "[name].css",
+                chunkFilename: "[id].css"
+            })
+        ]
+    },
+    {
+        // 入口文件
+        entry: {
+            pageThree: './src/pageThree/pageThree.js'
+        },
+        output: {
+            filename: 'script/[name].js',
+            path: path.resolve(__dirname, 'dist'),
+        },
+        module: _const_module,
+        plugins: [
+            new HtmlWebPackPlugin({
+                files: {
+                    js: ["script/pageThree.js"]
+                },
+                template: "./src/pageThree/pageThree.html",
+                filename: "./pageThree/pageThree.html"
+            }),
+            new MiniCssExtractPlugin({
+                filename: "[name].css",
+                chunkFilename: "[id].css"
+            })
+        ]
+    }
+    ];
